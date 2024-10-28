0e352f94a81d86883f01e3095f2ab471247b5f6b
Abdallah El-Sakka <37080741+al-sakka@users.noreply.github.com>
2024-10-28 03:52:55 +0200

    Update uart.h

--- MCAL/UART/uart.h
+++ MCAL/UART/uart.h
@@ -1,14 +1,14 @@
- /******************************************************************************
- *
- * Module: UART
- *
- * File Name: uart.h
- *
- * Description: Header file for the UART AVR driver
- *
- * Author: Mohamed Tarek
- *
- *******************************************************************************/
+/******************************************************************************
+*
+* Module: UART
+*
+* File Name: uart.h
+*
+* Description: Header file for the AVR UART driver
+*
+* Author: Abdallah El-Sakka
+*
+*******************************************************************************/
 
 #ifndef UART_H_
 #define UART_H_
@@ -25,33 +25,49 @@
 #define ALARM_SIGNAL		(3)
 
 /*******************************************************************************
- *                      Functions Prototypes                                   *
+*                                Definitions                                   *
 *******************************************************************************/
 
 typedef enum
 {
+	/* UPM */
 	Disabled,
 	Even_Parity = 2,
 	Odd_Parity
-	
+
 } UART_ParityMode ;
 
 typedef enum
 {
+	/* USBS */
 	_1_bit,
 	_2_bit
-	
+
 } UART_StopBitMode;
+
+typedef enum
+{
+	/* UCSZ */
+	_5_bit,
+	_6_bit,
+	_7_bit,
+	_8_bit,
+	_9_bit = 7
+
+} UART_BitDataMode;
 
 typedef struct
 {
-	uint8 bit_data;
+	UART_BitDataMode bit_data;
 	UART_ParityMode parity;
 	UART_StopBitMode stop_bit;
 	uint32 baud_rate;
 
 } UART_ConfigType;
 
+/*******************************************************************************
+*                      Functions Prototypes                                    *
+*******************************************************************************/
 
 /*
  * Description :
@@ -72,18 +88,18 @@ void UART_sendByte(const uint8 data);
  * Description :
  * Functional responsible for receive byte from another UART device.
  */
-uint8 UART_recieveByte(void);
+uint8 UART_receiveByte(void);
 
 /*
  * Description :
  * Send the required string through UART to the other UART device.
  */
-void UART_sendString(const uint8 *Str);
+void UART_sendString(const uint8*);
 
 /*
  * Description :
  * Receive the required string until the '#' symbol through UART from the other UART device.
  */
-void UART_receiveString(uint8 *Str); // Receive until #
+void UART_receiveString(uint8*); // Receive until #
 
 #endif /* UART_H_ */
